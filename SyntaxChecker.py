6303699b715b12cacfbc5d6da10612120e81176a
Olga Smirnova <o.smirnova@itmatrix.ru>
2013-10-04 16:15:27 +0400

    add .pm files to perl_checker command

--- SyntaxChecker.py
+++ SyntaxChecker.py
@@ -6,6 +6,8 @@ class SyntaxChecker(sublime_plugin.EventListener):
 			view.window().run_command("ruby_checker", {"saving": True})
 		elif view.file_name()[-3:] == '.pl':
 			view.window().run_command("perl_checker", {"saving": True})
+		elif view.file_name()[-3:] == '.pm':
+			view.window().run_command("perl_checker", {"saving": True})
 		elif view.file_name()[-4:] == '.php' or view.file_name()[-8:] == '.php.inc':
 			view.window().run_command("php_checker", {"saving": True})
 		elif view.file_name()[-4:] == '.xml':
